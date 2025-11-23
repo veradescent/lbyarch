38c0e50d1ed426cdc8ba6c0ae03be59532cc9651
rovmont <rovin_montano@dlsu.edu.ph>
2025-11-23 18:30:38 +0800

    Working output

--- asmgrayscale.asm
+++ asmgrayscale.asm
@@ -1,6 +1,6 @@
 ; Calculate and convert pixel values from int to float
 section .data
-    divisor dq 255.0
+    divisor dd 255.0        ; Single-precision float (32-bit)
 
 section .text
     bits 64
@@ -24,6 +24,10 @@ imgCvtGrayInttoFloat:
     call malloc
     add rsp, 32
     
+    ; Check if malloc failed
+    test rax, rax
+    jz .malloc_failed      ; If NULL, return NULL
+    
     ; rax now holds pointer to float array
     mov r10, rax            ; r10 = pointer to output float array
     xor r11, r11            ; r11 = counter (i = 0)
@@ -39,7 +43,7 @@ imgCvtGrayInttoFloat:
     mov ecx, dword[rbx + r11*4]  
     
     ; Convert int to float: divide by 255.0
-    cvtsi2ss xmm0, rcx      ; xmm0 = (double)array[i]
+    cvtsi2ss xmm0, ecx      ; xmm0 = (float)array[i] (use 32-bit register)
     movss xmm1, [rel divisor]
     divss xmm0, xmm1        ; xmm0 = array[i] / 255.0
     
@@ -48,9 +52,16 @@ imgCvtGrayInttoFloat:
     inc r11
     jmp .convert_loop
     
+.malloc_failed:
+    xor rax, rax            ; Return NULL
+    jmp .cleanup
+    
 .done:
+    mov rax, r10            ; Return pointer to float array
+    
+.cleanup:
     pop r12
     pop rbx
-    pop rsp
-    pop rbp
+    mov rsp, rbp            ; Restore stack pointer
+    pop rbp                 ; Restore base pointer
     ret
